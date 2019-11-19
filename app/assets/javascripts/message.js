062b428ffb05e8e3ee9c6a92758f44ad39fadbd4
uesin <kazukihana87@gmail.com>
2019-11-19 16:09:02 +0900

    非同期通信修正１
    修正を行った

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ) {
+  message.image? image = <img src=${message.image} ></img>: "";
       var html =
        `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
@@ -16,28 +16,10 @@ $(function(){
               ${message.content}
             </p>
           </div>
-          <img src=${message.image} >
+          ${image}
         </div>`
       return html;
-    } else {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
+    } 
   }
 $('#new_message').on('submit', function(e){
   e.preventDefault();
