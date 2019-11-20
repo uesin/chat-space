503d9445e0a63c4d40e0b44f8fcb26c1f6479387
uesin <kazukihana87@gmail.com>
2019-11-20 12:14:52 +0900

    インクリメンタルサーチの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def edit
   end
+  def index
+    @users = User.search(params[:keyword], current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
   def update
     if current_user.update(user_params)
       redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
-
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
 end
